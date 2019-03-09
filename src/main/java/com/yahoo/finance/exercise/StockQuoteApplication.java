5d1606b42de60bfd536eeb63083398463bdd814f
Ebrima Tunkara <etunkara51@gmail.com>
2019-03-08 23:05:43 -0500

    Rename application

--- src/main/java/com/yahoo/finance/exercise/StockQuoteApplication.java
+++ src/main/java/com/yahoo/finance/exercise/StockQuoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YahooFinanceAssignmentOneApplication {
+public class StockQuoteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YahooFinanceAssignmentOneApplication.class, args);
+        SpringApplication.run(StockQuoteApplication.class, args);
     }
 
 }
--- src/test/java/com/yahoo/finance/exercise/StockQuoteApplicationTests.java
+++ src/test/java/com/yahoo/finance/exercise/StockQuoteApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class YahooFinanceAssignmentOneApplicationTests {
+public class StockQuoteApplicationTests {
 
     @Test
     public void contextLoads() {
